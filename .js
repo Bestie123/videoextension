5be2aa1826049c163f8b26a1caae0a239cefcf92
Bestie123 <misha1997-09@mail.ru>
2022-06-06 10:28:30 +0300

    Update .js

--- .js
+++ .js
@@ -1,19 +1,6 @@
-Array.prototype.difference = function symmetricDierence(a1){
-return this.filter(x => !a1.includes(x));
+Array.prototype.difference = function symmetricDierence(a1) { // проверяет есть ли в первом массиве совпадения со вторым массивом, удаляет из первого совпавшие элементы и возвращает первый массив
+    return this.filter(x => !a1.includes(x));
 }
-
-////////////////////  
-// Examples  
-////////////////////
-/*
-
-var dif1 = [1,2,3,4,5,6].diff( [3,4,5] );  
-console.log(dif1); // => [1, 2, 6]
-
-
-var dif2 = ["test1", "test2","test3","test4","test5","test6"].diff(["test1","test2","test3","test4"]);  
-console.log(dif2); // => ["test5", "test6"]
-*/
 anomfuncarr = [];
 setInterval(function() {
     anomfuncarr.shift()?.()
@@ -50,43 +37,29 @@ document.body.addEventListener("DOMNodeInserted", function(e) {
         e.target.addEventListener("click", function() {
             ee22 = Object.keys(checklist2);
             ee = Object.keys(araayvideolist)
-             loc1 = [];
-             loc2 = [];
+            loc1 = {};
             eeeegf = vkApi.api('video.get', {
                 videos: ee.toString()
             }, function(r) {});
 
             Promise.all([eeeegf]).then((values232) => {
                 values232[0].items.forEach((item, item2) => {
-                    if (item.can_add == 1) {
-                        var owner_id = item.owner_id,
-                            id_video = item.id,
-                            parent_owner_id = values232[0].items[item2 - 1]?.owner_id,
-                            parent_id_video = values232[0].items[item2 - 1]?.id;
-
-                        loc1.push({
+                    var owner_id = item.owner_id,
+                        id_video = item.id,
+                        parent_owner_id = values232[0].items[item2 - 1]?.owner_id,
+                        parent_id_video = values232[0].items[item2 - 1]?.id,
+                        pointervidobj = loc1[owner_id + '_' + id_video] = {
                             owner_id: owner_id,
                             id_video: id_video,
                             parent_owner_id: parent_owner_id,
-                            parent_id_video: parent_id_video
-                        })
-                    } else {
-                        var owner_id = item.owner_id,
-                            id_video = item.id,
-                            parent_owner_id = values232[0].items[item2 - 1]?.owner_id,
-                            parent_id_video = values232[0].items[item2 - 1]?.id;
-                        loc2.push({
-                            hash: araayvideolist[String(owner_id) + "_" + String(id_video)],
-                            owner_id: owner_id,
-                            id_video: id_video,
-                            parent_owner_id: parent_owner_id,
-                            parent_id_video: parent_id_video
-                        })
-
+                            parent_id_video: parent_id_video,
+                            newaddedalbums: {},
+                            can_add: item.can_add
+                        }
+                    if (item.can_add == 0) {
+                        pointervidobj.hash = araayvideolist[String(owner_id) + "_" + String(id_video)]
                     }
                 });
-                console.log(loc1)
-                console.log(loc2)
                 llll3 = [];
                 ee22.forEach((item) => {
                     checklist2[item].split(" ").forEach((item2) => {
@@ -101,140 +74,154 @@ document.body.addEventListener("DOMNodeInserted", function(e) {
                 ee23.forEach((item3) => {
                     llll4.push(parseInt(item3.split("wrap")[1], 10));
                 })
-
-                loc2.forEach((item,indx) => {
-                    //  llll1 = item.split("_")[0] // ид владельца
-                    // llll2 = item.split("_")[1] // ид видео
-                    var darsi = function() {
-                        var a2 = item.owner_id; //parseInt(llll1, 10);
-                        var a3 = item.id_video; //parseInt(llll2, 10);
-                        var a4 = llll3;
-                        var a1 = item;
-                        var anomfunc2 = function() {
-                            console.log(a4);
-                            ajax.post('/al_video.php', {
-                                act: 'a_add_to_playlist',
-                                oid: a2,
-                                vid: a3, //
-                                playlists: a4,
-                                hash: item.hash
-                            },{onDone: function(playlistIds) {
-								loc2[indx].newaddedalbums = a4.difference(playlistIds);
-                  console.log(playlistIds) // -2 -1 1 2 показывает в каких альбомах видео уже было.................................из  a4 вычитаем playlistIds и получаем в какие альбомы видео сейчас добавлено
-                }})
+                ee67 = Object.keys(loc1);
+                if (ee67.length > 0) {
+                    ee67.forEach((item, indx) => {
+                        if (loc1[item].can_add == 0) {
+                            var darsi = function() {
+                                var index = indx;
+                                var a2 = loc1[item].owner_id;
+                                var a3 = loc1[item].id_video;
+                                var a4 = llll3;
+                                var a1 = loc1[item];
+                                var anomfunc2 = function() {
+                                    ajax.post('/al_video.php', {
+                                        act: 'a_add_to_playlist',
+                                        oid: a2,
+                                        vid: a3, //
+                                        playlists: a4,
+                                        hash: loc1[item].hash
+                                    }, {
+                                        onDone: function(playlistIds) {
+                                            loc1[item].newaddedalbums = a4.difference(playlistIds).reduce((a, v) => ({
+                                                ...a,
+                                                [v]: ''
+                                            }), {});
+                                            console.log(playlistIds) // -2 -1 1 2 показывает в каких альбомах видео уже было.................................из  a4 вычитаем playlistIds и получаем в какие альбомы видео сейчас добавлено
+                                            if (ee67.length == indx + 1) {
+                                                // вызываем следующую функцию
+                                                function2();
+                                            };
+                                        }
+                                    })
+                                }
+                                anomfuncarr.push(anomfunc2);
+                            };
+                            darsi();
+                        } else {
+                            function2();
                         }
-                        anomfuncarr.push(anomfunc2);
-                    };
-                    darsi();
-                })
+                    })
+                } else {
+                    function2();
+                }
+                function2 = function() {
+                    ee68 = Object.keys(loc1);
+                    if (ee68.length > 0) {
+                        ee68.forEach((item, indx) => {
+                            if (loc1[item].can_add == 1) {
+                                var darsi = function() {
+                                    var a1 = llll4;
+                                    var a4 = llll3;
+                                    var a2 = loc1[item].owner_id;
+                                    var a3 = loc1[item].id_video;
+                                    var anomfunc2 = function() {
+                                        eeeegf = vkApi.api('video.addToAlbum', {
+                                            album_ids: a4.toString(),
+                                            owner_id: a2,
+                                            video_id: a3,
+                                            v: "5.81"
+                                        }, function(r) {});
+                                        Promise.all([eeeegf]).then((values) => {
+                                            loc1[item].newaddedalbums = values[0].reduce((a, v) => ({
+                                                ...a,
+                                                [v]: ''
+                                            }), {});
+                                            // показывает в какие альбомы добавлено видео сейчас..............................................
+                                            console.log(values.length);
+                                            if (ee68.length == indx + 1) {
+                                                function3();
+                                            }
+                                        })
+                                    }
+                                    var anomfunc222 = function() {
+                                        eeeegf = vkApi.api('video.removeFromAlbum', {
+                                            album_ids: a1.toString(),
+                                            owner_id: a2,
+                                            video_id: a3,
+                                            v: "5.81"
+                                        }, function(r) {});
 
-
-                loc1.forEach((item,indx) => {
-                    var darsi = function() {
-                        var a1 = llll4;
-                        var a4 = llll3;
-                        var a2 = item.owner_id;
-                        var a3 = item.id_video;
-                        var anomfunc2 = function() {
-
-                            eeeegf = vkApi.api('video.addToAlbum', {
-                                album_ids: a4.toString(),
-                                owner_id: a2,
-                                video_id: a3,
-                                v: "5.81"
-                            }, function(r) {});
-
-                            Promise.all([eeeegf]).then((values) => {
-								loc1[indx].newaddedalbums = values[0];
-								 // показывает в какие альбомы добавлено видео сейчас..............................................
-								console.log(5656)
-                                console.log(values.length);
-                            })
-                        }
-                        var anomfunc222 = function() {
-                            eeeegf = vkApi.api('video.removeFromAlbum', {
-                                album_ids: a1.toString(),
-                                owner_id: a2,
-                                video_id: a3,
-                                v: "5.81"
-                            }, function(r) {});
-
-                            Promise.all([eeeegf]).then((values) => {
-								    console.log(values);
-                            })
-                        }
-                        anomfuncarr.push(anomfunc2);
-                        anomfuncarr.push(anomfunc222);
-
+                                        Promise.all([eeeegf]).then((values) => {
+                                            console.log(values);
+                                        })
+                                    }
+                                    anomfuncarr.push(anomfunc2);
+                                    anomfuncarr.push(anomfunc222);
+                                }
+                                darsi();
+                            } else {
+                                if (ee68.length == indx + 1) {
+                                    function3();
+                                }
+                            }
+                        })
+                    } else {
+                        function3();
                     }
-                    darsi();
-                })
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				loc2.forEach((item,indx) => {
-					// перебирать параметр newaddedalbums и устанавливать в качестве album_id и сравнивать в loc1 и loc2 по иерархии (after_owner_id и after_video_id) вверх, если есть совпадение, то брать видео after_owner_id и after_video_id
-					// добавить сортировку по выбранным видео исползуя порядковый номер childlist либо при запросе информации о всех видео в альбоме 
-					eeeegf = vkApi.api('video.reorderVideos', {
-                                album_id: album_id,
-                                owner_id: item.owner_id,  //
-								video_id: item.id_video,  //
-								after_owner_id: after_owner_id, // 
-								after_video_id: after_video_id
-                            }, function(r) {});
+                }
+                function3 = function() {
 
-                            Promise.all([eeeegf]).then((values) => {
-                                console.log(values);
-                            })
-				})
-				
-				loc1.forEach((item,indx) => {
-				eeeegf = vkApi.api('video.reorderVideos', {
-                                album_id: album_id,
-                                owner_id: owner_id,
-								video_id: video_id,
-								after_owner_id: after_owner_id,
-								after_video_id: after_video_id
-                            }, function(r) {});
-
-                            Promise.all([eeeegf]).then((values) => {
-                                console.log(values);
-                            })	
-				});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                    listItem2 = document.getElementById("video_all_list").children;
+                    listArray2 = [...listItem2];
+                    parentvideosarray = [];
+                    listArray2.forEach((item1) => {
+                        ggggg = item1.getAttribute("ckeckn1");
+                        if (ggggg == "null" || ggggg == null || ggggg == 0 || ggggg == "0" || ggggg == '0') {} else {
+                            if (parentvideosarray.length > 0) {
+                                // есть элемент, смотрим в каких альбомах он есть, проверяем есть ли в этом альбоме родительское видео, если да, то сортируем, если нет,  то берем более высокий родительский элемент и проверяем
+                                Object.keys(loc1[item1.getAttribute("data-id")].newaddedalbums).forEach((item2) => {
+                                    parentvideoindxcount = parentvideosarray.length;
+                                    nestedparentvideo = function() {
+                                        if (parentvideoindxcount > 0) {
+                                            parentvideoindxcount = parentvideoindxcount - 1;
+                                            if (item2 in parentvideosarray[parentvideoindxcount].newaddedalbums) {
+                                                var loc444 = function() {
+                                                    var a6 = item1;
+                                                    var a1 = item2, // перебирается для конкретного видео
+                                                        a2 = loc1[a6.getAttribute("data-id")].owner_id,
+                                                        a3 = loc1[a6.getAttribute("data-id")].id_video,
+                                                        a4 = parentvideosarray[parentvideoindxcount].owner_id,
+                                                        a5 = parentvideosarray[parentvideoindxcount].id_video
+                                                    var loc333 = function() {
+                                                        eeeegf = vkApi.api('video.reorderVideos', {
+                                                            album_id: a1,
+                                                            owner_id: a2,
+                                                            video_id: a3,
+                                                            after_owner_id: a4,
+                                                            after_video_id: a5
+                                                        }, function(r) {});
+                                                        Promise.all([eeeegf]).then((values) => {
+                                                            console.log(values);
+                                                        })
+                                                    }
+                                                    anomfuncarr.push(loc333);
+                                                }
+                                                loc444();
+                                            } else {
+                                                nestedparentvideo()
+                                            }
+                                        }
+                                    }
+                                    nestedparentvideo();
+                                })
+                                parentvideosarray.push(loc1[item1.getAttribute("data-id")]);
+                            } else {
+                                parentvideosarray.push(loc1[item1.getAttribute("data-id")]);
+                            }
+                        }
+                    });
+                }
             })
         })
     }
@@ -298,19 +285,15 @@ document.getElementById("video_all_list").addEventListener("DOMNodeInserted", fu
     4 добавление всех видео при нажатии кнопки сохранить +
     5 полезность данного скрипта в том, что не требуется обращаться к api запросам разработчика
 6 скрипт имеет преимущество над api разработчиков т к там при обращении к приватным видео выдается ошибка доступа и ничего не сделаешь, данный скрипт не выдает ошибки
-
 1 присвоить к идентификатору каждой записи[хеш, идентификатор доступа] +
 2 разбить записи на два массива - один для api, второй для приватных видео +
 3 перебрать каждый массив и добавить видеозаписи в альбомы +
 4 с помощью api узнать в каких альбомах видео уже есть, восстановить порядковый номер видеозаписей без перемещения тех которые уже находятся в альбомах
 если видео уже находилось в альбоме и идет попытка сортировки, игнорировать сортировку, сортировку пробрасыват на предыдущее видео
-
-
 первый метод если cann_add = 0 = [
     [ид владельца, ид видео, хеш, ид видео за которым находилось в массиве, ид владельца за которым находилось в массиве]
 ]
 второй метод если cann add = 1 = [
     [ид владельца, ид видео, ид видео за которым находилось в массиве, ид владельца за которым находилось в массиве]
 ]
-
 */
